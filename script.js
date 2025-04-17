1e4a77dec41a0c067b160b7d6918ef44f9ad4a6d
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-17 18:09:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let isConfirm = confirm("Do you want to proceed?")
+
+if(isConfirm){
+	alert("You Clicked OK. Proceeding...");
+} else {
+	alert("You Clicked Cancel. Exiting...");
+}
+
